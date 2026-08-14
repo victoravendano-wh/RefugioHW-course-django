aaf7cae96ee79c23e7c59deb329ff0dbd91208e3
victor-wisphub <victor_armando@wisphub.net>
2026-08-14 14:02:54 -0500

    Se registran los modelos en admin.site para poder administrar las DB desde el panel web

--- apps/adopciones/admin.py
+++ apps/adopciones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from apps.adopciones.models import Persona
+
+admin.site.register(Persona)
--- apps/mascotas/admin.py
+++ apps/mascotas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apps.mascotas.models import Vacuna, Mascota
+
 # Register your models here.
+
+
+admin.site.register(Vacuna)
+admin.site.register(Mascota)
