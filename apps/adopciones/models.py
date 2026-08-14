90a2dd4117ee41054f33da2cddd2b1244814ef7f
victor-wisphub <victor_armando@wisphub.net>
2026-08-14 12:26:53 -0500

    Se realizan los primeros modelos en las apps mascotas y adopciones

--- apps/adopciones/models.py
+++ apps/adopciones/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+	nombre = models.CharField(max_length=50)
+	apellidos = models.CharField(max_length=50)
+	edad = models.IntegerField()
+	telefono = models.CharField(max_length=60)
+	email = models.EmailField()
+	domicilio = models.TextField()
