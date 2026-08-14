dd2fb638152b03abf04db78f9f6cc3a2e953a455
victor-wisphub <victor_armando@wisphub.net>
2026-08-14 12:41:19 -0500

    Se crean relaciones ManyToMany y OneToOne con on_delete_ CASCADE

--- apps/mascotas/models.py
+++ apps/mascotas/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+from apps.adopciones.models import Persona
+
 # Create your models here.
 
+class Vacuna(models.Model):
+	nombre = models.CharField(max_length=6)
 
 class Mascota(models.Model):
 	nombre = models.CharField(max_length=50)
 	sexo = models.CharField(max_length=60)
 	edad_aproximada = models.IntegerField()
 	fecha_rescate = models.DateField()
+	persona = models.ForeignKey(Persona, null = True, on_delete = models.CASCADE, blank=True)
+	vacuna = models.ManyToManyField(Vacuna, null=True, blank=True)
