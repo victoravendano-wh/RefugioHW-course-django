fd77d687606ab3096ba643362d09d1ca041c961f
victor-wisphub <victor_armando@wisphub.net>
2026-08-17 16:05:22 -0500

    Se agregan registros de solicitud de adopcion, editarlos, eliminarlos, con vistas en base a clases

--- apps/adopciones/urls.py
+++ apps/adopciones/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 
-from apps.adopciones.views import SolicitudList, SolicitudCrear
+from apps.adopciones.views import SolicitudList, SolicitudCrear, SolicitudUpdate, SolicitudEliminar
+
 
 urlpatterns = [
 	url(r'^solicitud/listar', SolicitudList.as_view(), name='solicitud_listar'),
-	url(r'^solicitud/crear', SolicitudCrear.as_view(), name='solicitud_crear')
+	url(r'^solicitud/crear', SolicitudCrear.as_view(), name='solicitud_crear'),
+    url(r"^solicitud/editar/(?P<pk>\d+)$", SolicitudUpdate.as_view(), name="solicitud_actualizar"),
+	url(r'^solicitud/eliminar/(?P<pk>\d+)$', SolicitudEliminar.as_view(), name="solicitudBorrar")
 ]
--- apps/adopciones/views.py
+++ apps/adopciones/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse_lazy
 
 # Create your views here.
 
-from apps.adopciones.models import Solicitud
+from apps.adopciones.models import Solicitud, Persona
 from apps.adopciones.forms import PersonaForms, SolicitudForm
 
 
@@ -39,3 +39,43 @@ class SolicitudCrear(CreateView):
 			return HttpResponseRedirect(self.get_success_url())
 		else:
 			return self.render_to_response(self.get_context_data(form=form, form2=form2))
+
+class SolicitudUpdate(UpdateView):
+	model= Solicitud
+	second_model = Persona
+	template_name = 'adopcion/solicitud_form.html'
+	form_class = SolicitudForm
+	second_form_class = PersonaForms
+	success_url = reverse_lazy('app_adopciones:solicitud_listar')
+
+	def get_context_data(self, **kwargs):
+		context = super(SolicitudUpdate, self).get_context_data(**kwargs)
+		pk = self.kwargs.get('pk', 0)
+		solicitud = self.model.objects.get(id = pk)
+		persona = solicitud.persona
+		if 'form' not in context:
+			context['form'] = self.form_class()
+		if 'form2' not in context:
+			context['form2'] = self.second_form_class(instance=persona)
+		context['id'] = pk
+		return context;
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object
+		id_solicitud = kwargs['pk']
+		solicitud = self.model.objects.get(id=id_solicitud)
+		persona = solicitud.persona
+		form = self.form_class(request.POST, instance = solicitud)
+		form2 = self.second_form_class(request.POST, instance=persona)
+		if form.is_valid() and form2.is_valid():
+			form.save()
+			form2.save()
+			return HttpResponseRedirect(self.get_success_url())
+		else:
+			return HttpResponseRedirect(self.get_success_url())
+
+class SolicitudEliminar(DeleteView):
+	model = Solicitud
+	template_name = 'adopcion/solicitud_eliminar.html'
+	success_url = reverse_lazy('app_adopciones:solicitud_listar')
+	
